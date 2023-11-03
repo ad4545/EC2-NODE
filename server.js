f3844d49f359bb142f81db87633a5c0633248eb9
EC2 Default User <ec2-user@ip-10-0-1-215.ap-south-1.compute.internal>
2023-11-03 13:42:20 +0000

    second commit

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = express();
 const cors = require("cors");
 const dotenv = require("dotenv");
 const { Kafka } = require("kafkajs");
+const { createServer } = require('node:http');
+const server = createServer(app)
+
 
 dotenv.config();
 app.use(express.json());
@@ -15,8 +18,7 @@ app.use(
 const kafka = new Kafka({
   clientId: "node-producer",
   brokers: [
-    "b-1-public.kafkamskcluster.e0b8oe.c2.kafka.ap-south-1.amazonaws.com:9196",
-    "b-2-public.kafkamskcluster.e0b8oe.c2.kafka.ap-south-1.amazonaws.com:9196",
+    "b-2.kafkamskcluster.e0b8oe.c2.kafka.ap-south-1.amazonaws.com:9096","b-1.kafkamskcluster.e0b8oe.c2.kafka.ap-south-1.amazonaws.com:9096"
   ],
   sasl: {
     mechanism: "scram-sha-512",
@@ -26,34 +28,17 @@ const kafka = new Kafka({
   ssl: true,
 });
 
-var max_battery = 0;
-var winner_robot = "";
-
-const batteryConsumer = kafka.consumer({ groupId: "group-1" });
 const taskProducer = kafka.producer({
   allowAutoTopicCreation: false,
   transactionTimeout: 30000,
 });
 
-async function checkBattery() {
-  await batteryConsumer.run({
-    eachMessage: async ({ topic, partition, message }) => {
-      const msg = JSON.parse(message.value.toString());
-      const data = msg["data"];
-      const battery = data.battery;
-      if (battery > max_battery) {
-        max_battery = battery;
-        winner_robot = data.robot_name;
-      }
-    },
-  });
-}
+const batteryConsumer = kafka.consumer({ groupId: "group-2" });
 
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
 
-const server = app.listen(8000, () => [console.log("connected")]);
 
 const io = require("socket.io")(server, {
   cors: {
@@ -61,22 +46,29 @@ const io = require("socket.io")(server, {
   },
 });
 
-io.on("connection", (socket) => {
-  socket.on("setup", async (mssg) => {
-    console.log(mssg.id);
-    await batteryConsumer.connect();
-    await taskProducer.connect();
-    await batteryConsumer.subscribe({ topics: ["battery"] });
-    checkBattery();
-  });
-  socket.on("task", async (task) => {
-    console.log(winner_robot);
-    task.robot_name = winner_robot;
-    const task_to = JSON.stringify(task);
-    await taskProducer.send({
-      topic: "task",
-      messages: [{ key: "", value: task_to }],
-    });
-    console.log("process done");
-  });
+let socket = io.on("connection", (socket) => {
+  socket.on('task',async(msg)=>{
+      let finalMsg = JSON.stringify(msg.task)
+      await taskProducer.send({
+        topic:`task_${msg.robot}`,
+        messages:[{key:'',value:finalMsg}],
+        ttl:5000
+      })
+      console.log('task published')
+  })
+   return socket
 });
+
+server.listen(9000, async() => {
+  console.log("connected")
+  await taskProducer.connect();
+  await batteryConsumer.connect()
+  await batteryConsumer.subscribe({topics:['battery']})
+  await batteryConsumer.run({
+    eachMessage:async({topic,partition,message})=>{
+      let msg = message.value.toString()
+      socket.emit('robot-battery',msg)
+      console.log(msg,'battery')
+    }
+  })
+}); 
